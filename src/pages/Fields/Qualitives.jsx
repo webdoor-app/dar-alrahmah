3c8874d508c69df8fa41d0a96185ceeb136d0ca2
Prince_Rd <moon260097@gmail.com>
2025-02-25 23:11:46 +0300

    Fields/1 Update

--- src/pages/Fields/Qualitives.jsx
+++ src/pages/Fields/Qualitives.jsx
@@ -4,44 +4,69 @@ import Footer from "../../components/Footer";
 import ServicesSection from "../../components/ServicesSection";
 import UrbenFoundationSection from "../../components/UrbenFoundationSection";
 import UrgenPlaningIMage from "../../assets/img/urban-planning.png";
+import mobileFrame from "../../assets/img/mobileFrame.svg";
+import urbanPlanningEffect from "../../assets/img/Urben-Ellipse.svg";
 
 function Qualitives() {
   return (
     <div>
       <Header />
 
-
+      <div className="reltive  justify-start items-center md:hidden  ">
+        <img
+          src={mobileFrame}
+          alt="icon"
+          className="flex md:hidden absolute left-0 top-[42%]  "
+        />
+      </div>
 
       {/*Hero section  */}
       <section className="h- bg-accent font-camel">
         <div className="md:flex items-center justify-center gap-6 pt-20  md:m-base-m mx-5">
           {/* Image */}
 
-          <div className=" text p-0 m-0 ">
+          <div className="  text-center  mt-6 md:mt-0 p-0 m-0 ">
             <img
               src={UrgenPlaningIMage}
-              alt=''
-              className=" md:w-[30rem] mx-auto md:mx-0  w-[20rem]    animate-float animation-delay-2000"
+              alt=""
+              className=" md:w-[30rem] mx-auto md:mx-0  w-[10rem] animate-float animation-delay-2000"
             />
-           
+            <img
+              src={urbanPlanningEffect}
+              alt=""
+              className=" md:w-[30rem] mx-auto md:mx-0 md:hidden flex  w-[10rem] animate-float animation-delay-2000"
+              loading=""
+            />
           </div>
 
           {/* content */}
 
           <div className="pt">
-            <h1 className="text-primary font-extrabold text-2xl md:text-6xl mb-4 mt-20 md:mt-40"> الخبرات النوعية</h1>
-            <p className="text-secondary font-bold justify-evenly text-justify w-full md:w-[90%] text-sm md:text-lg ">
-            نتميز بخبرات نوعية تجمع بين تطوير مرافق ضيوف الرحمن لضمان الراحة والكفاءة من خلال حلول مبتكرة تخدم الحجاج والمعتمرين، ودراسة وإدارة حركة الحشود لضمان سلامتهم وانسيابية التنقل في التجمعات الكبرى، بالإضافة إلى تصميم وتأهيل الوجهات السياحية والتاريخية بما يبرز الهوية الثقافية والتراثية ويحقق التوازن بين الاستدامة وجذب الزوار لتقديم تجارب مميزة ومتكاملة.
+            <h1 className="text-primary font-extrabold text-[21px] md:text-[76.24px] md:mb-0 mb-8 mt-8 md:mt-40">
+              {" "}
+              الخبرات النوعية
+            </h1>
+            <p className="text-secondary  font-bold justify-evenly text-justify w-full md:w-[90%] text-sm md:text-[20px]/7 ">
+              نتميز بخبرات نوعية تجمع بين تطوير مرافق ضيوف الرحمن لضمان الراحة
+              والكفاءة من خلال حلول مبتكرة تخدم الحجاج والمعتمرين، ودراسة وإدارة
+              حركة الحشود لضمان سلامتهم وانسيابية التنقل في التجمعات الكبرى،
+              بالإضافة إلى تصميم وتأهيل الوجهات السياحية والتاريخية بما يبرز
+              الهوية الثقافية والتراثية ويحقق التوازن بين الاستدامة وجذب الزوار
+              لتقديم تجارب مميزة ومتكاملة.
             </p>
           </div>
         </div>
       </section>
 
-
       <section>
-      <UrbenFoundationSection tabsToShow={["تصميم الوجهات التاريخية والسياحية", "تصميم وتخطيط حركة الحشود", "تطوير مرافق منظومة ضيوف الرحمن"]} />
+        <UrbenFoundationSection
+          tabsToShow={[
+            "تصميم الوجهات التاريخية والسياحية",
+            "تصميم وتخطيط حركة الحشود",
+            "تطوير مرافق منظومة ضيوف الرحمن",
+          ]}
+        />
       </section>
-
 
       {/* <ServicesSection /> */}
       <Footer />
