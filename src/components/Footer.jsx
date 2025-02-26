415b11d1105a0a09f8a01c4a11c8bc08c39d9a5e
Prince_Rd <moon260097@gmail.com>
2025-02-26 22:21:27 +0300

    Footer Update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,18 +5,26 @@ import footerIcons from "../assets/img/footer-icons.svg";
 import mobileFooterIcon1 from "../assets/img/MobileFooterIcon1.svg";
 import mobileFooterIcon2 from "../assets/img/MobileFooterIcon2.svg";
 import logoLight from "../assets/img/logoLight.svg";
+import FooterDesktopIconR from "../assets/img/FooterDesktopIconR.svg";
+import FooterDesktopIconL from "../assets/img/FooterDesktopIconL.svg";
 
 function Footer() {
   return (
-    <footer className="relative h-[] bg-primaryBurnt text-white font-camel overflow-hidden">
+    <footer className="relative md:h-[27%] bg-primaryBurnt text-white font-camel overflow-hidden">
       {/* Background Image */}
-      <div className="absolute inset-0 -top-5 z-0">
+      <div className="absolute inset-0 top-0 z-0">
         {/* Desktop */}
-        <div className="hidden md:flex justify-between items-center h-full">
+        <div className="hidden  md:flex justify-between items-center w-full">
           <img
-            src={footerIcons}
+            src={FooterDesktopIconL}
             alt="Footer Icons"
-            className="w-full h- object-cover"
+            className="w- h-full object-cover"
+            loading="lazy"
+          />
+          <img
+            src={FooterDesktopIconR}
+            alt="Footer Icons"
+            className="w- h-full object-cover"
             loading="lazy"
           />
         </div>
@@ -39,7 +47,7 @@ function Footer() {
 
       {/* Content */}
       <div className="relative md:w-full w- z-10 container w-fit justify-center items-center text-center mx-auto px-">
-        <h3 className="font-sm py-4 text-gray-400 text-nowrap">
+        <h3 className="font-sm text-xs md:text-lg py-4 text-gray-400 text-nowrap">
           سياسة الخصوصية
           <span className="mx-3">|</span>
           شروط الاستخدام
@@ -50,7 +58,7 @@ function Footer() {
           <div className="space-y-4">
             <a href="#">
               <img
-                className="w-64"
+                className="w-56 md:w-64"
                 src={logoLight}
                 alt="Logo"
                 loading="lazy"
@@ -60,44 +68,10 @@ function Footer() {
 
           {/* Social Media Section */}
           <div className="space-y-2">
-            <h3 className="text-xl font-bold">تواصل معنا</h3>
-            <div className="flex gap-4 justify-center">
-              {/* X (Twitter) */}
-              <a
-                href="#"
-                className="text-secondary size-5 hover:text-white transition-colors"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-                </svg>
-              </a>
-
-              {/* Phone */}
-              <a
-                href="#"
-                className="text-secondary size-5 hover:text-white transition-colors"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path d="M2.25 6.75c0 8.284 6.716 15 15 15h1.5a.75.75 0 0 0 .75-.75v-4.5a.75.75 0 0 0-.75-.75h-3.379a.75.75 0 0 0-.53.22l-2.126 2.125a20.42 20.42 0 0 1-6.232-6.232l2.125-2.126a.75.75 0 0 0 .22-.53V3.75a.75.75 0 0 0-.75-.75H3.75a.75.75 0 0 0-.75.75v1.5z" />
-                </svg>
-              </a>
-
+            
+            <h3 className="text-xl text-center text-accent    hidden md:flex items-center justify-center text[29.12px] font-bold"> تواصـل معنـــــا</h3>
+            <h3 className=" md:hidden  flex items-center justify-center text[12px] font-bold"> حساباتنــا على مواقــع التواصل الإجتماعي </h3>
+            <div className="flex flex-row-reverse gap-8 md:gap-4 justify-center">
               {/* Mail */}
               <a
                 href="#"
@@ -116,6 +90,64 @@ function Footer() {
                   <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908z" />
                 </svg>
               </a>
+               {/* Phone */}
+               <a
+                href="#"
+                className="text-secondary size-5 hover:text-white transition-colors"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M2.25 6.75c0 8.284 6.716 15 15 15h1.5a.75.75 0 0 0 .75-.75v-4.5a.75.75 0 0 0-.75-.75h-3.379a.75.75 0 0 0-.53.22l-2.126 2.125a20.42 20.42 0 0 1-6.232-6.232l2.125-2.126a.75.75 0 0 0 .22-.53V3.75a.75.75 0 0 0-.75-.75H3.75a.75.75 0 0 0-.75.75v1.5z" />
+                </svg>
+              </a>
+
+               {/* Instagram */}
+               <a
+                href="#"
+                className="text-secondary size-5 hover:text-white transition-colors"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <rect x="2" y="2" width="20" height="20" rx="5" ry="5" />
+                  <path d="M16 11.37a4 4 0 1 1-5.33 0" />
+                </svg>
+              </a>
+
+
+
+              {/* X (Twitter) */}
+              <a
+                href="#"
+                className="text-secondary size-5 hover:text-white transition-colors"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                </svg>
+              </a>
+
+             
 
               {/* LinkedIn */}
               <a
@@ -135,13 +167,14 @@ function Footer() {
                   <path d="M8 11v5M8 8v.01M12 16v-5m4 5v-3a2 2 0 0 0-4 0" />
                 </svg>
               </a>
+             
             </div>
           </div>
         </div>
 
         {/* Copyright Section - Centered */}
-        <div className="flex justify-center mt-6 py-4">
-          <div className="w-fit text-sm text-center">
+        <div className="flex justify-center mt-6 py-4 md:py-8">
+          <div className="w-fit text-xs md:text-lg text-center">
             <p className="text-gray-400 text-nowrap">
               &copy; {new Date().getFullYear()} جميع الحقوق محفوظة لدار الرحمة
               لـلاستشـارات
