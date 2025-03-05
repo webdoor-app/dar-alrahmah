4bd25b4dc8153dc931658252fce0b3a3ab2921e2
Prince_Rd <inbox.info30@gmail.com>
2025-03-05 23:36:27 +0300

    ProjectHeaderMobile Update

--- src/components/ProjectMainHeader.jsx
+++ src/components/ProjectMainHeader.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 // Import all images
 import projectHeader1 from "../assets/img/ProjectHeader1.svg";
+import projectHeaderMobile1 from "../assets/img/ProjectHeaderMobile1.svg";
 import arrowDownIcon from "../assets/img/ArrowDownIcon.svg";
 import plusIcon from "../assets/img/PlusIcon.svg";
 import dimond from "../assets/img/Dimond.svg";
@@ -56,8 +57,8 @@ export class ProjectMainHeader extends Component {
     return (
       <div>
         <section className="font-camel h-fit bg-accent">
-          {/* Section Header */}
-          <div className="flex pt-5 md:pt-12 justify-between relative">
+          {/* Section Header desktop */}
+          <div className="hidden md:flex pt- md:pt-12 justify-between relative">
             {/* Left Side with ServicesHeader1 and h2 */}
             <div className="w-[30%] md: lg:w-fit  mt-4 relative">
               <img
@@ -66,20 +67,35 @@ export class ProjectMainHeader extends Component {
                 loading="lazy"
               />
               {/* Absolute h2 */}
-              <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/4 -translate-y-1/2 text-lg md:text-3xl lg:text-[76.24px] font-bold text-tertiary mb-6">
+              <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/4 -translate-y-1/2 text-[21px] font-extrabold md:text-3xl lg:text-[76.24px] md:font-bold text-tertiary mb-6">
+              أعمالنــــــا
+              </h1>
+            </div>
+          </div>
+          {/* Section Header mobile */}
+          <div className="flex   md:hidden pt- md:pt-12 justify-between relative">
+            {/* Left Side with ServicesHeader1 and h2 */}
+            <div className="w- md: lg:w-fit  mt-4 relative">
+              <img
+                src={projectHeaderMobile1}
+                alt="Services Header mobile 1"
+                loading="lazy"
+              />
+              {/* Absolute h2 */}
+              <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/4 -translate-y-1/2 text-[21px] font-extrabold md:text-3xl lg:text-[76.24px] md:font-bold text-tertiary mb-6">
               أعمالنــــــا
               </h1>
             </div>
           </div>
 
           {/* Projects section */}
-          <div className=" py-5 md:py-20">
+          <div className=" py-5 mt-7 md:mt-0 md:py-20">
             <div className="  rounded-xl md:rounded-bl-3xl h-fit bg-white mx-4 md:mx-8">
               {/* top */}
               <div className=" flex  items-center justify-">
                 <div className=" card-container   bg-accent rounded-bl-xl md:rounded-bl-3xl">
                   <div className="card-header">
-                    <h1 className="font-extrabold px-8  md:pt-10 p-3 md:p-5 md:px-10 text-lg md:text-[47.12px] text-tertiary">
+                    <h1 className="font-extrabold px-  md:pt-10 px-3 py-2 md:p-5 md:px-10 text-[18px] md:text-[47.12px] text-tertiary">
                       دار الرحمة في أرقام
                     </h1>
                   </div>
