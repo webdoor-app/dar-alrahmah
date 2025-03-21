5325109d4fa963951686148b1af2a22f33fa34f2
Prince_Rd <inbox.info30@gmail.com>
2025-03-21 23:56:20 +0300

    Contact config

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -82,9 +82,14 @@ function Contact() {
     setIsSubmitting(true);
 
     emailjs
-      .sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, { // Emialjs configuration
-        publicKey: 'YOUR_PUBLIC_KEY',
-      })
+    .sendForm(
+      import.meta.env.VITE_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      form.current,
+      {
+        publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY,
+      }
+    )
       .then(
         () => {
           console.log('SUCCESS!');
